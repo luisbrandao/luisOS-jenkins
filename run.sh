f0ef442b5133bf6da879dd865122f8ac1a2b8151
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2018-08-04 16:50:41 -0300

    Change restart policy

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -d --name luisos-jenkins -p 8080:8080 --restart always -v jenkins_data:/var/lib/jenkins luisos-jenkins
+docker run --privileged -d --name luisos-jenkins -p 8080:8080 --restart unless-stopped -v jenkins_data:/var/lib/jenkins luisos-jenkins
