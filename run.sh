fa8e6ba3f6a8dfbd3576cd57047def0930296192
Luis Alexandre Deschamps Brandão <luis.brandao@agrotis.com>
2019-01-18 14:58:55 -0200

    Improved run script

--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-docker run --privileged -d --name luisos-jenkins -p 2222:22 -p 8080:8080 --restart unless-stopped -v jenkins_data:/var/lib/jenkins luisos-jenkins
+var=$(docker volume ls | grep jenkins_data)
+if [ -z "$var" ] ; then
+  docker volume create jenkins_data
+else
+  echo "Jenkins volume alerady exists"
+fi
+
+docker run --privileged -d --name luisos-jenkins \
+                           --restart unless-stopped \
+                            -p 2222:22 \
+                            -p 8080:8080 \
+                            -v jenkins_data:/var/lib/jenkins \
+                            luisos-jenkins
