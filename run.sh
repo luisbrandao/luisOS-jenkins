0a7d3675de7d14a2d954509239e012da0b7513c7
Luis Alexandre Deschamps Brandão <luis.brandao@agrotis.com>
2019-04-16 12:46:11 -0300

    Update run script

--- run.sh
+++ run.sh
@@ -6,8 +6,10 @@ else
   echo "Jenkins volume alerady exists"
 fi
 
-docker run --privileged -d --name luisos-jenkins \
-                           --restart unless-stopped \
-                            -p 8080:8080 \
-                            -v jenkins_data:/var/lib/jenkins \
-                            luisos-jenkins
+docker run --privileged -d \
+  --name luisos-jenkins \
+  --restart unless-stopped \
+  -p 8080:8080 \
+  -v jenkins_data:/var/lib/jenkins \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  registry.techsytes.com/luisos-jenkins:latest
